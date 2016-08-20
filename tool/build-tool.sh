36efeab4a680907fb6abb04942383dcf947434e4
Burt P <pburt0@gmail.com>
2016-08-20 08:33:43 -0500

    bin name change

--- tool/build-tool.sh
+++ tool/build-tool.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-gcc hdcd-detect.c wavreader.c ../hdcd_decode2.c -lm -o hdcd_detect
+gcc hdcd-detect.c wavreader.c ../hdcd_decode2.c -lm -o hdcd-detect
+rm -f hdcd-detect.o wavreader.o
