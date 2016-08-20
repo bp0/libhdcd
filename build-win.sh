34f8d40c58e17961e1f1b859d797bf80f9ff3982
Burt P <pburt0@gmail.com>
2016-08-20 08:34:45 -0500

    add script to build windows binary

---
+++ build-win.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+LIBNAME=libhdcd_decode2
+
+mkdir -p win-bin
+cd win-bin
+
+MGCC=i686-w64-mingw32-gcc
+"$MGCC" -c ../hdcd_decode2.c
+"$MGCC" -shared -o $LIBNAME.dll hdcd_decode2.o -Wl,--out-implib,$LIBNAME.a
+rm -f hdcd_decode2.o
+
+"$MGCC" -c ../tool/hdcd-detect.c ../tool/wavreader.c
+"$MGCC" -o hdcd-detect.exe hdcd-detect.o wavreader.o -L. -l$LIBNAME
+rm -f hdcd-detect.o wavreader.o
+
+cd ..
+
+
+
