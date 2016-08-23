0c6d3b4026f38c0e855075268ebb3129f2f358e3
Burt P <pburt0@gmail.com>
2016-08-23 11:48:26 -0500

    Update build-win.sh

--- build-win.sh
+++ build-win.sh
@@ -6,9 +6,9 @@ mkdir -p win-bin
 cd win-bin
 
 MGCC=i686-w64-mingw32-gcc
-"$MGCC" -c ../hdcd_decode2.c
-"$MGCC" -shared -o $LIBNAME.dll hdcd_decode2.o -Wl,--out-implib,$LIBNAME.a
-rm -f hdcd_decode2.o
+"$MGCC" -c ../hdcd_decode2.c ../hdcd_simple.c ../hdcd_libversion.c
+"$MGCC" -shared -o $LIBNAME.dll hdcd_decode2.o  hdcd_libversion.o  hdcd_simple.o -Wl,--out-implib,$LIBNAME.a
+rm -f hdcd_decode2.o hdcd_simple.o hdcd_libversion.o
 
 "$MGCC" -c ../tool/hdcd-detect.c ../tool/wavreader.c
 "$MGCC" -o hdcd-detect.exe hdcd-detect.o wavreader.o -L. -l$LIBNAME
