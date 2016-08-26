e8ca287dfbf3fa29359b0ab4dbedb2a98c09001b
Burt P <pburt0@gmail.com>
2016-08-26 08:03:08 -0500

    build-win.sh: create a zip file of the binaries

--- build-win.sh
+++ build-win.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# build a windows binary package
+
 MGCC=i686-w64-mingw32-gcc
 MWINDRES=i686-w64-mingw32-windres
 LIBNAME=libhdcd
 
+if [ -z `which zip` ]; then echo "Needs zip"; exit 1; fi
+if [ -z `which unix2dos` ]; then echo "Needs unix2dos"; exit 1; fi
 if [ -z `which perl` ]; then echo "Needs perl"; exit 1; fi
 if [ -z `which "$MGCC"` ]; then echo "Needs mingw gcc"; exit 1; fi
 if [ -z `which "$MWINDRES"` ]; then echo "Needs mingw windres"; exit 1; fi
@@ -65,5 +69,35 @@ rm -f hdcd-detect.o wavreader.o wavout.o
 
 rm -f "libhdcd.res" "hdcd-detect.res" "hdcd.res"
 rm -f "libhdcd.res.rc" "hdcd-detect.res.rc" "hdcd.res.rc"
+
+PKN="libhdcd-win-$PVER"
+mkdir -p $PKN
+cat << EOF > "$PKN/FILES.txt"
+libhdcd
+A library for High Definition Compatible Digital (HDCD) decoding and analysis.
+https://github.com/bp0/libhdcd
+
+Package version: win-$PVER
+
+Files:
+libhdcd.dll	- libhdcd shared library
+hdcd-detect.exe	- HDCD detection/decoding tool
+hdcd.exe	- version of the decoding tool that attempts to
+........	  be compatible with Key's original hdcd.exe
+LICENSE.txt	- license(s)
+AUTHORS.txt	- authors
+FILES.txt	- list of files in the package
+EOF
+cp "hdcd.exe" "$PKN"
+cp "hdcd-detect.exe" "$PKN"
+cp "libhdcd.dll" "$PKN"
+cp ../LICENSE "$PKN/LICENSE.txt"
+cp ../AUTHORS "$PKN/AUTHORS.txt"
+unix2dos -o "$PKN/LICENSE.txt"
+unix2dos -o "$PKN/AUTHORS.txt"
+unix2dos -o "$PKN/FILES.txt"
+zip "$PKN.zip" "$PKN/hdcd.exe" "$PKN/hdcd-detect.exe" "$PKN/libhdcd.dll" "$PKN/LICENSE.txt" "$PKN/AUTHORS.txt" "$PKN/FILES.txt"
+rm -f "$PKN/hdcd.exe" "$PKN/hdcd-detect.exe" "$PKN/libhdcd.dll" "$PKN/LICENSE.txt" "$PKN/AUTHORS.txt" "$PKN/FILES.txt"
+rmdir "$PKN"
 
 cd ..
